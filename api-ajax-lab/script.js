45cc8609e1591435c3cff35ce790526adb34c905
Renee Paczkowski <rpaczkowski@gmail.com>
2022-06-09 16:06:32 -0400

    Posts popping in, but as undefined

--- api-ajax-lab/script.js
+++ api-ajax-lab/script.js
@@ -1,12 +1,25 @@
-let postContainer = document.createElement("div");
+const container = document.getElementById("post-container");
 
-const responses = [];
+const postArray = [];
 
-fetch("https://www.reddit.com/r/aww.json")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
+fetch("https://www.reddit.com/r/aww/.json")
+  .then((response) => {
+    return response.json();
   })
-  .catch((err) => {
-    console.log(err);
+  .then((myJson) => {
+    let jsonResults = myJson.data.children;
+    console.log(jsonResults);
+    jsonResults.forEach((post) => {
+      let div = document.createElement("div");
+      let h3 = document.createElement("h3");
+      let image = document.createElement("img");
+      let link = document.createElement("a");
+      image.src = post.thumbnail;
+      h3.innerText = post.title;
+      link.src = post.permalink;
+      div.appendChild(image);
+      div.appendChild(h3);
+      div.classList.add("posts");
+      container.appendChild(div);
+    });
   });
